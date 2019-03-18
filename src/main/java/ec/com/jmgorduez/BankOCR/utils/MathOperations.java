d5c1e7dabf914a8de52a3f97030c45a659d0f56e
jmgorduez <jmgorduez@gmail.com>
2019-03-18 16:41:12 -0500

    MathOperation class implemented

--- src/main/java/ec/com/jmgorduez/BankOCR/utils/MathOperations.java
+++ src/main/java/ec/com/jmgorduez/BankOCR/utils/MathOperations.java
@@ -1,28 +1,23 @@
 package ec.com.jmgorduez.BankOCR.utils;
 
-import ec.com.jmgorduez.BankOCR.domain.Digit;
-
-import java.text.DecimalFormat;
-import java.util.Arrays;
-
 import static ec.com.jmgorduez.BankOCR.utils.Constants.*;
 
 public class MathOperations {
     private final static int TEN = 10;
 
-    public static int binaryToDecimal(Integer[] binaryNumber){
-        return arrayToNumber(binaryNumber, TWO);
+    public static int bitsArrayToNumberBaseTen(Integer[] binaryNumber){
+        return arrayToNumberAccordingBase(binaryNumber, TWO);
     }
 
-    public static int intArrayToDecimal(Integer[] binaryNumber){
-        return arrayToNumber(binaryNumber, TEN);
+    public static int digitsArrayToNumberBaseTen(Integer[] binaryNumber){
+        return arrayToNumberAccordingBase(binaryNumber, TEN);
     }
 
-    private static int arrayToNumber(Integer[] arrayNumber, int base){
+    private static int arrayToNumberAccordingBase(Integer[] arrayNumber, int numberBase){
         double number = 0;
         int position = arrayNumber.length - ONE;
         for (int digit : arrayNumber){
-            number = digit * Math.pow(base,position) + number;
+            number = digit * Math.pow(numberBase,position) + number;
             position--;
         }
         return (int) number;
--- src/test/java/ec/com/jmgorduez/BankOCR/utils/MathOperationsTest.java
+++ src/test/java/ec/com/jmgorduez/BankOCR/utils/MathOperationsTest.java
@@ -14,20 +14,20 @@ class MathOperationsTest {
     @Test
     @DisplayName("It should convert int[] that represent a binary number to a decimal number")
     void binaryToDecimal() {
-        assertThat(MathOperations.binaryToDecimal(ONE_BASE_TWO_ARRAY))
+        assertThat(MathOperations.bitsArrayToNumberBaseTen(ONE_BASE_TWO_ARRAY))
                 .isEqualTo(ONE);
-        assertThat(MathOperations.binaryToDecimal(THREE_BASE_TWO_ARRAY))
+        assertThat(MathOperations.bitsArrayToNumberBaseTen(THREE_BASE_TWO_ARRAY))
                 .isEqualTo(THREE);
-        assertThat(MathOperations.binaryToDecimal(FIVE_BASE_TWO_ARRAY))
+        assertThat(MathOperations.bitsArrayToNumberBaseTen(FIVE_BASE_TWO_ARRAY))
                 .isEqualTo(FIVE);
-        assertThat(MathOperations.binaryToDecimal(ONE_BASE_TWO_ARRAY))
+        assertThat(MathOperations.bitsArrayToNumberBaseTen(ONE_BASE_TWO_ARRAY))
                 .isNotEqualTo(TWO);
     }
 
     @Test
     @DisplayName("It should convert int[] that represent a decimal number to a decimal number")
     void intArrayToDecimal(){
-        assertThat(MathOperations.intArrayToDecimal(BINARY_CODE_ONE_ARRAY))
+        assertThat(MathOperations.digitsArrayToNumberBaseTen(BINARY_CODE_ONE_ARRAY))
                 .isEqualTo(BINARY_CODE_ONE);
     }
 }
