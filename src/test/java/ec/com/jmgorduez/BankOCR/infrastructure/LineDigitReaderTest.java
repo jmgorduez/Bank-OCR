61a8feb94266188fc7f514d71b06b236db35561a
jmgorduez <jmgorduez@gmail.com>
2019-03-18 16:45:14 -0500

    LineDigitReaderTest class implemented

---
+++ src/test/java/ec/com/jmgorduez/BankOCR/infrastructure/LineDigitReaderTest.java
@@ -0,0 +1,17 @@
+package ec.com.jmgorduez.BankOCR.infrastructure;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LineDigitReaderTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void readLine() {
+    }
+}
