ed105cdc9d08104f6068967d2cfa6f139090f6b0
jmgorduez <jmgorduez@gmail.com>
2019-03-20 12:11:59 -0500

    Increasing test coverage

--- src/test/java/ec/com/jmgorduez/BankOCR/infrastructure/DigitReaderTest.java
+++ src/test/java/ec/com/jmgorduez/BankOCR/infrastructure/DigitReaderTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static ec.com.jmgorduez.BankOCR.DataTestGenerator.*;
+import static ec.com.jmgorduez.BankOCR.dataGenerator.DataTestGenerator.*;
 import static ec.com.jmgorduez.BankOCR.utils.Constants.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
