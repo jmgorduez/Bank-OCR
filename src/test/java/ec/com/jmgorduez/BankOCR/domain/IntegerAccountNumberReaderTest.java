3eee6adbbdac4fde0fccce4dcc7464629e82fa9d
jmgorduez <jmgorduez@gmail.com>
2019-03-20 15:47:51 -0500

    Test implemented

---
+++ src/test/java/ec/com/jmgorduez/BankOCR/domain/IntegerAccountNumberReaderTest.java
@@ -0,0 +1,48 @@
+package ec.com.jmgorduez.BankOCR.domain;
+
+import ec.com.jmgorduez.BankOCR.domain.abstractions.IMultilineStringReader;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.io.IOException;
+
+import static ec.com.jmgorduez.BankOCR.dataGenerator.DataTestGenerator.accountNumber111111111;
+import static ec.com.jmgorduez.BankOCR.dataGenerator.DataTestGenerator.generateListDigitsOne;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+class IntegerAccountNumberReaderTest {
+
+    private IntegerAccountNumberReader integerAccountNumberReaderUnderTest;
+    @Mock
+    private IMultilineStringReader<Integer, DigitToken.TokenType> multilineStringReaderMock;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        integerAccountNumberReaderUnderTest = new IntegerAccountNumberReader();
+        try {
+            when(multilineStringReaderMock.read(any(), any(), any()))
+                    .thenReturn(generateListDigitsOne());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    @DisplayName("It should read a account number from a characters list.")
+    void readAccountNumber() {
+        try {
+            assertThat(integerAccountNumberReaderUnderTest
+                                .readAccountNumber(
+                                        multilineStringReaderMock.read(any(), any(), any())))
+                    .isEqualTo(accountNumber111111111());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
