57cfd22d468d2a1a5c586b5f9854b7948536e97d
jmgorduez <jmgorduez@gmail.com>
2019-03-25 14:19:55 -0500

    Code refactoring

--- src/test/java/ec/com/jmgorduez/BankOCR/BankOcrApplicationTests.java
+++ src/test/java/ec/com/jmgorduez/BankOCR/BankOcrApplicationTests.java
@@ -20,10 +20,9 @@ import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 public class BankOcrApplicationTests {
 
-    //private static final String FILE_PATH = "C:\\Users\\JuanMa\\projects\\java\\Bank-OCR\\inputFiles\\input.txt";
-    private static final String FILE_PATH_111111111 = "/home/jm/projects/java/Bank-OCR/inputFiles/input111111111.txt";
-    private static final String FILE_PATH_123456789 = "/home/jm/projects/java/Bank-OCR/inputFiles/input123456789.txt";
-    private static final String FILE_PATH_49006771_ = "/home/jm/projects/java/Bank-OCR/inputFiles/input49006771_.txt";
+    private static final String FILE_PATH_111111111 = FILE_PATH + "input111111111.txt";
+    private static final String FILE_PATH_123456789 = FILE_PATH + "input123456789.txt";
+    private static final String FILE_PATH_49006771_ = FILE_PATH + "input49006771_.txt";
     private IMultilineStringReader multilineStringReader;
     private ILineReader<DigitToken.TokenType> lineReader;
     private IMultilineCharacterReader<Integer, DigitToken.TokenType> multilineCharacterReader;
